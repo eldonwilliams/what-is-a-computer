77eb2efb75cf7e548c43ea2e6e1760eb662c3478
MrTops <43430084+MrTops@users.noreply.github.com>
2022-02-23 21:53:44 -0500

    move to desktop

--- src/SlideDisplay.js
+++ src/SlideDisplay.js
@@ -1,18 +1,63 @@
-import { Button, ButtonGroup, Paper, Typography } from "@mui/material";
-import { Motion, spring } from "react-motion";
+import { Button, ButtonGroup, Paper, Stack, Typography } from "@mui/material";
+import { AnimatePresence, motion, useMotionValue, useTransform } from "framer-motion";
+import CheckIcon from '@mui/icons-material/Check';
+import { useEffect, useState } from "react";
 
 const Slide = ({ slides, currentSlide, handleNext, handlePrevious, slide, index }) => {
+    const x = useMotionValue(0);
+    const height = useTransform(x, [-65, -50, 0, 50, 65], [-15, 0, 0, 0, -15]);
+    const [signalState, setSignalState] = useState(0); // 0 - invisible | 1 - checkmark | 2 - no
 
-    return (<Motion defaultStyle={{'opacity': 0}} style={{ 'opacity': spring(currentSlide === index ? 1 : 0, { 'stiffness': 250, 'damping': 22.5, }), }}>
-        {(style) => (<Paper sx={{ 'position': 'absolute', 'top': '15%', 'width': 'content', 'maxWidth': '300px', 'height': 'content', 'padding': '10px', 'opacity': style.opacity, 'transform': `scale(${style.opacity})`, }} elevation={5}>
-            <Typography variant="h5"><b>{slide.label}</b></Typography><br/>
-            <Typography>{slide.content}</Typography><br/>
-            <ButtonGroup>
-                <Button onClick={handlePrevious} disabled={(slides.length - 1 === currentSlide && currentSlide === 0) || currentSlide === 0}>Previous</Button>
-                <Button onClick={handleNext} variant="contained" disabled={slides.length - 1 === currentSlide}>Next</Button>
-            </ButtonGroup>
-        </Paper>)}
-    </Motion>)
+    const dragEnd = () => {
+        if (x.get() > 50 && index + 1 < slides.length) handleNext();
+        if (x.get() < -50 && index - 1 >= 0) handlePrevious();
+    }
+
+    useEffect(() => x.onChange(() => {
+        // This is condensed into a single line of ? operator logic just to make it shorter, but it *should* work
+        setSignalState(x.get() === 0 ? 0 : (x.get() > 50 || x.get() < -50 ? 1 : 2));
+    }), []);
+
+    return (<AnimatePresence>
+        {currentSlide === index && <motion.div
+            initial={{ opacity: 0, scale: 0.75, }}
+            animate={{ opacity: 1, scale: 1, }}
+            exit={{ opacity: 0, scale: 0.75, }}
+            drag="x"
+            dragConstraints={{ 'left': 0, 'right': 0, 'top': 0, 'bottom': 0, }}
+            style={{
+                x,
+                'position': 'absolute',
+                'top': '25%', 'width': 'content',
+                'maxWidth': '300px', 'height': 'content',
+                'y': height,
+            }}
+            onDragEnd={dragEnd}
+        >
+            <Paper
+                elevation={5}
+                sx={{ 'padding': '10px', }}
+            >
+                <Typography variant="h5"><b>{slide.label}</b></Typography><br/>
+                <Typography>{slide.content}</Typography><br/>
+                <Stack
+                    direction="row"
+                    alignItems="center"
+                    justifyContent="space-between"
+                >
+                    <ButtonGroup>
+                        <Button onClick={handlePrevious} disabled={(slides.length - 1 === currentSlide && currentSlide === 0) || currentSlide === 0}>Previous</Button>
+                        <Button onClick={handleNext} variant="contained" disabled={slides.length - 1 === currentSlide}>Next</Button>
+                    </ButtonGroup>
+                    <div>
+                        <AnimatePresence>
+                            
+                        </AnimatePresence>
+                    </div>
+                </Stack>
+            </Paper>
+        </motion.div>}
+    </AnimatePresence>);
 }
 
 const SlideDisplay = ({ slides, currentSlide, setSlide, }) => {
