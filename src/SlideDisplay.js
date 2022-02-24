ab7f9110e50f162af09fa579c6fa4860f990f976
MrTops <eldon.ew.com@gmail.com>
2022-02-24 08:54:51 -0500

    Update SlideDisplay.js

--- src/SlideDisplay.js
+++ src/SlideDisplay.js
@@ -1,12 +1,34 @@
 import { Button, ButtonGroup, Paper, Stack, Typography } from "@mui/material";
 import { AnimatePresence, motion, useMotionValue, useTransform } from "framer-motion";
 import CheckIcon from '@mui/icons-material/Check';
+import CloseIcon from '@mui/icons-material/Close';
+import DoDisturbOnIcon from '@mui/icons-material/DoDisturbOn';
+import DragIndicatorIcon from '@mui/icons-material/DragIndicator';
 import { useEffect, useState } from "react";
+
+/**
+ * @type {import("framer-motion").Transition}
+ */
+const transition = {
+    type: 'spring',
+    duration: 0.25,
+}
+
+const iconVariants = {
+    "active": {
+        opacity: 1, scale: 1,
+        transition,
+    },
+    "inactive": {
+        opacity: 0, scale: 0,
+        transition,
+    },
+}
 
 const Slide = ({ slides, currentSlide, handleNext, handlePrevious, slide, index }) => {
     const x = useMotionValue(0);
     const height = useTransform(x, [-65, -50, 0, 50, 65], [-15, 0, 0, 0, -15]);
-    const [signalState, setSignalState] = useState(0); // 0 - invisible | 1 - checkmark | 2 - no
+    const [signalState, setSignalState] = useState(0); // 0 - prompt | 1 - checkmark | 2 - no
 
     const dragEnd = () => {
         if (x.get() > 50 && index + 1 < slides.length) handleNext();
@@ -15,6 +37,8 @@ const Slide = ({ slides, currentSlide, handleNext, handlePrevious, slide, index 
 
     useEffect(() => x.onChange(() => {
         // This is condensed into a single line of ? operator logic just to make it shorter, but it *should* work
+        if (x.get() > 0 && index + 1 >= slides.length) { setSignalState(0); return; }
+        if (x.get() < 0 && index - 1 < 0) { setSignalState(0); return; }
         setSignalState(x.get() === 0 ? 0 : (x.get() > 50 || x.get() < -50 ? 1 : 2));
     }), []);
 
@@ -49,9 +73,46 @@ const Slide = ({ slides, currentSlide, handleNext, handlePrevious, slide, index 
                         <Button onClick={handlePrevious} disabled={(slides.length - 1 === currentSlide && currentSlide === 0) || currentSlide === 0}>Previous</Button>
                         <Button onClick={handleNext} variant="contained" disabled={slides.length - 1 === currentSlide}>Next</Button>
                     </ButtonGroup>
-                    <div>
+                    <div
+                        style={{
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignContent: 'center',
+                            height: '100%',
+                        }}
+                    >
                         <AnimatePresence>
-                            
+                            {signalState === 2 && <motion.div
+                                variants={iconVariants}
+                                initial="inactive"
+                                animate="active"
+                                exit="inactive"
+                                style={{ position: 'absolute', }}
+                            >
+                                <CloseIcon color="error" />
+                            </motion.div>}
+                        </AnimatePresence>
+                        <AnimatePresence>
+                            {signalState === 1 && <motion.div
+                                variants={iconVariants}
+                                initial="inactive"
+                                animate="active"
+                                exit="inactive"
+                                style={{ position: 'absolute', }}
+                            >
+                                <CheckIcon color="success" />
+                            </motion.div>}
+                        </AnimatePresence>
+                        <AnimatePresence>
+                            {signalState === 0 && <motion.div
+                                variants={iconVariants}
+                                initial="inactive"
+                                animate="active"
+                                exit="inactive"
+                                style={{ position: 'absolute', }}
+                            >
+                                <DragIndicatorIcon />
+                            </motion.div>}
                         </AnimatePresence>
                     </div>
                 </Stack>
