2677ecc9db4f4782975b398bebf35f8ef7023cf1
MrTops <43430084+MrTops@users.noreply.github.com>
2022-03-18 11:03:57 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,36 +6,14 @@ import './App.css';
 
 const slides = [
   {
-    "label": "CPU",
-    "content": "The CPU is the \"Brain\" of the computer. The CPU is really good at doing math, fast.",
+    "label": "Creation",
+    "title": "How was Eldon Slides made?",
+    "content": "Ms. Gary has requested I talk about how Eldon Slides was created, the software used to create it, and how it works in layman's terms. The following slides will cover that.",
   },
   {
-    "label": "RAM",
-    "content": "The RAM, or Random Access Memory, is the part of the computer which remembers things short-term.",
-  },
-  {
-    "label": "Hard Drive",
-    "content": "The Hard Drive is used to save picture, games, and other big files. The Hard Drive has a lot more space than the RAM which is why you want to use it for bigger files.",
-  },
-  {
-    "label": "Motherboard",
-    "content": "The Motherboard connects all the other parts of the computer. It holds the CPU, RAM, GPU, and most major components.",
-  },
-  {
-    "label": "GPU",
-    "content": "The GPU is useful for rendering video games. It can handle the math that each pixel needs for realistic games. GPUs are also useful for rendering videos when editing.",
-  },
-  {
-    "label": "Case",
-    "content": "The Case of a computer is like your skin. It holds all the components together and allows you to transport them without breaking them.",
-  },
-  {
-    "label": "Power Supply",
-    "content": "The Power Supply of a computer is like your stomach. It provides energy to all components which need it. They're often very inexpensive and not that complex, but they're important!",
-  },
-  {
-    "label": "Uses",
-    "content": "The four major uses of a computer are taking inputs, storing data, processing data, and generating outputs. The way computers do this can vary a lot, but they all do those 4 functions in some way.",
+    "label": "Software",
+    "title": "What was Eldon Slides made with?",
+    "content": "Ms. Gary has requested I talk about how Eldon Slides was created, the software used to create it, and how it works in layman's terms. The following slides will cover that.",
   },
 ]
 
